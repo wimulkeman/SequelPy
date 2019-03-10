f7c24a56aaba5bcaaf6ee9e52d591764b1d54579
Wim Ulkeman <wimulkeman@hotmail.com>
2019-03-10 18:10:05 +0100

    Displaying a text editor
    The text editor will be the base to run SQL queries in

--- sequel_py.py
+++ sequel_py.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 import wx
 
+
+class LandingFrame(wx.Frame):
+
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.Show(True)
+
+
 app = wx.App(False)
-frame = wx.Frame(None, wx.ID_ANY, 'SequelPy')
-frame.Show(True)
+frame = LandingFrame(None, 'SequelPy')
 app.MainLoop()
